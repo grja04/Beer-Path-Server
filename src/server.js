d3c0653334b529814b011bdaa7c6dd278b303bfe
Crismoo <mooaguilarcris@gmail.com>
2021-02-23 11:20:27 -0600

    first commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const kodersRouter = require('./routes/koders')
+const authRouter = require('./routes/auth')
+const server = express()
+
+server.use(cors())
+server.use(express.json()) // Middleware
+server.use('/koders', kodersRouter)
+server.use('/auth', authRouter)
+
+server.get('/', (request, response) => {
+  response.json({
+    success: true,
+    message: 'ChelaApp API'
+  })
+})
+
+module.exports = server
